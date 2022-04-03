bca834961921670a2ad5c87fbbf39c5177133fc6
Виктория <zero-zuzcuc@yandex.ru>
2022-04-04 01:19:25 +0600

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,6 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 
-export default name;
+export default () => {
+  console.log(`Hello, ${name}!`);
+};
