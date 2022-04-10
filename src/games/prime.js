087b8be01b6b60855575815b5fda9e3bcef54ed0
Виктория <zero-zuzcuc@yandex.ru>
2022-04-11 00:56:43 +0600

    fixes

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,14 +3,15 @@ import randomNum from '../utils.js';
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  let result = '';
+  if (number < 2) {
+    return 'no';
+  }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
-      result = 'no';
-      break;
-    } else result = 'yes';
+      return 'no';
+    }
   }
-  return result;
+  return 'yes';
 };
 
 const generateData = () => {
