b3878a3f908675285c2c219c20a319ecae7c3f54
Виктория <zero-zuzcuc@yandex.ru>
2022-04-13 01:05:18 +0600

    fixes

--- src/games/even.js
+++ src/games/even.js
@@ -6,7 +6,7 @@ const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const generateData = () => {
-  const num = randomNum(103);
+  const num = randomNum(200);
   const answer = isEven(num) ? 'yes' : 'no';
   console.log(`Question: ${num}`);
   return answer;
